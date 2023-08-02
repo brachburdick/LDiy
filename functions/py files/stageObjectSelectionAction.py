16b7f9eb528282a58d9d287f1ac030de95178bd2
Brach Burdick <107267496+brachburdick@users.noreply.github.com>
2023-08-02 06:48:36 -0400

    Delete functions directory

--- functions/py files/stageObjectSelectionAction.py
+++
@@ -1,6 +0,0 @@
-
-# Function to combine Group, Fixture and Projector tables into StageHierarchy
-def selectionUpdate(selectionPath, selectionName):
-    op('stageCont/Util')['selected', 'stageData'] = selectionName
-    return
-            
